a423d1e5166e9f93abe63689b1d288dd6afab948
Lorraine Martin <lolocodes2001@gmail.com>
2023-10-31 11:34:27 -0700

    added SitterDetailsPage, added fields to reg page, changed HomePage to ClientHomePage, SearchPage

---
+++ src/App.js
@@ -0,0 +1,38 @@
+// General Imports
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+// Pages Imports
+import ClientHomePage from "./pages/ClientHomePage/ClientHomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+
+// Component Imports
+import Navbar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+
+// Util Imports
+import PrivateRoute from "./utils/PrivateRoute";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <ClientHomePage />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/RegisterPage/RegisterPage.js
@@ -0,0 +1,109 @@
+import React, { useContext } from "react";
+import AuthContext from "../../context/AuthContext";
+import useCustomForm from "../../hooks/useCustomForm";
+
+const RegisterPage = () => {
+  const { registerUser } = useContext(AuthContext);
+  const defaultValues = {
+    username: "",
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+  };
+  const [formData, handleInputChange, handleSubmit] = useCustomForm(
+    registerUser,
+    defaultValues
+  );
+
+  return (
+    <div className="container">
+      <form className="form" onSubmit={handleSubmit}>
+        <label>
+          Username:{" "}
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          First Name:{" "}
+          <input
+            type="text"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Last Name:{" "}
+          <input
+            type="text"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Email:{" "}
+          <input
+            type="text"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Password:{" "}
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Street Address:{" "}
+          <input
+            type="text"
+            name="streetAddress"
+            value={formData.streetAddress}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          City:{" "}
+          <input
+            type="text"
+            name="city"
+            value={formData.city}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Zipcode:{" "}
+          <input
+            type="text"
+            name="zipCode"
+            value={formData.zipCode}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Phone Number:{" "}
+          <input
+            type="text"
+            name="phoneNumber"
+            value={formData.phoneNumber}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button>Register!</button>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterPage;
