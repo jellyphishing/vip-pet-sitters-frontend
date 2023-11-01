016559ea604c6f1de3f9b33a3bba94d5da658af0
Lorraine Martin <lolocodes2001@gmail.com>
2023-11-01 12:34:00 -0700

    reviewform logic

--- src/components/ReviewForm/ReviewForm.jsx
+++ src/components/ReviewForm/ReviewForm.jsx
@@ -1,5 +1,50 @@
-const ReviewForm = () => {
-  return <div></div>;
+import React from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "../..hooks/useAuth";
+import useCustomForm from "../../hooks/useCustomForm";
+
+const addReviewPage = (props) => {
+  const [user, token] = useAuth();
+  const navigate = useNavigate();
+
+  const initialValues = {
+    text: "",
+  };
+  const [formData, handleInputChange, handleSubmit, reset] = useCustomForm(
+    postNewReview,
+    initialValues
+  );
+
+  async function postNewReview() {
+    console.log("Form Submitted");
+  }
+  //add axios post request here
+  const postNewReview = async () => {
+    try {
+      let response = await axios.post(`https://localhost:5001/api/reviews/`);
+      setNewReview(response.data.clientId);
+    } catch (error) {
+      console.log("Error in post new review: ", error);
+    }
+  };
+  return (
+    <div className="container">
+      <h1>Share your thoughts! Write a review!</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <label>
+          Text:{" "}
+          <input
+            type="text"
+            name="text"
+            value={formData.text}
+            onChange={handleInputChange}
+          />
+        </label>
+      </form>
+      <button>Add New Review</button>
+    </div>
+  );
 };
 
-export default ReviewForm;
+export default addReviewPage;
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -19,33 +19,56 @@ const LoginPage = () => {
   }, [isServerError]);
 
   return (
-    <div className="container">
-      <form className="form" onSubmit={handleSubmit}>
-        <label>
-          Username:{" "}
-          <input
-            type="text"
-            name="userName"
-            value={formData.userName}
-            onChange={handleInputChange}
-          />
-        </label>
-        <label>
-          Password:{" "}
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-          />
-        </label>
-        {isServerError ? (
-          <p className="error">Login failed, incorrect credentials!</p>
-        ) : null}
-        <Link to="/registerSitter">Click to register as a sitter!</Link>
-        <Link to="/registerClient">Click to register as a client!</Link>
-        <button>Login!</button>
-      </form>
+    <div>
+      <div>
+        <h1>Hello! Welcome to VIP Pet Sitters!</h1>
+        <p>
+          We are so glad you stopped by!There are many pet sitting companies out
+          there nowadays, so why choose us? We require our sitters to carry
+          insurance so you can be rest assured that should an issue arise, you
+          are covered. We learned the hard way and not only did it cost us
+          financially, our pet paid the price. This is why we created VIP Pet
+          Sitters!
+        </p>
+        <p>
+          Need to find a pet sitter for your animal and struggling to find the
+          right fit? Maybe you have a high maintenace bully breed? Or an exotic
+          pet that takes medication? Do you need services in our home or maybe
+          you want us to come to you in your house or hotel? Are you being
+          deployed and need someone who will safely and lovingly foster your pet
+          so you don't have to worry while you are away serving our country? We
+          are here for you!
+        </p>
+      </div>
+
+      <div className="container">
+        <form className="form" onSubmit={handleSubmit}>
+          <label>
+            Username:{" "}
+            <input
+              type="text"
+              name="userName"
+              value={formData.userName}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Password:{" "}
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+            />
+          </label>
+          {isServerError ? (
+            <p className="error">Login failed, incorrect credentials!</p>
+          ) : null}
+          <Link to="/registerSitter">Click to register as a sitter!</Link>
+          <Link to="/registerClient">Click to register as a client!</Link>
+          <button>Login!</button>
+        </form>
+      </div>
     </div>
   );
 };
